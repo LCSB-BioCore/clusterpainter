8b2af897fad411262b6eccf2beafa37530311369
Mirek Kratochvil <exa.exa@gmail.com>
2025-05-13 11:36:06 +0200

    make it work with glossy rendering

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,6 @@ import DearImGui
 import DearImGui.OpenGL3
 import DearImGui.SDL
 import DearImGui.SDL.OpenGL
-
 import Graphics.GL
 import SDL
 
@@ -12,6 +11,10 @@ import Control.Exception (bracket, bracket_)
 import Control.Monad (unless, when)
 import Control.Monad.IO.Class ()
 import Control.Monad.Managed
+
+import qualified Graphics.Gloss as G
+import Graphics.Gloss.Data.Color
+import Graphics.Gloss.Rendering
 
 main :: IO ()
 main = do
@@ -22,39 +25,49 @@ main = do
         let title = "Hello, Dear ImGui!"
         let config =
               defaultWindow
-                {windowGraphicsContext = OpenGLContext defaultOpenGL}
+                { windowGraphicsContext = OpenGLContext defaultOpenGL
+                , windowResizable = True
+                }
         managed $ bracket (createWindow title config) destroyWindow
     glContext <- managed $ bracket (glCreateContext window) glDeleteContext
     _ <- managed $ bracket createContext destroyContext
     managed_ $ bracket_ (sdl2InitForOpenGL window glContext) sdl2Shutdown
     managed_ $ bracket_ openGL3Init openGL3Shutdown
-    liftIO $ mainLoop window
+    st <- liftIO $ initState
+    liftIO $ mainLoop st window
 
-mainLoop :: Window -> IO ()
-mainLoop window =
-  unlessQuit $ do
-  -- Tell ImGui we're starting a new frame
+mainLoop :: State -> Window -> IO ()
+mainLoop st window = do
+  finished <- handleEvents
+  unless finished $ do
+    -- start frame
     openGL3NewFrame
     sdl2NewFrame
     newFrame
+    -- rendering
+    (V2 wsx wsy) <- fmap fromIntegral <$> get (windowSize window)
+    displayPicture (wsx, wsy) (greyN 0.25) st 1.0 (Circle 30)
+    -- UI
     withWindowOpen "Hello, ImGui!" mainWidget
     showDemoWindow
-    glClear GL_COLOR_BUFFER_BIT
+    -- UI rendering
     render
     openGL3RenderDrawData =<< getDrawData
+    -- post-frame
     glSwapWindow window
-    mainLoop window
+    mainLoop st window
   where
-  -- Process the event loop
-    unlessQuit action = do
-      shouldQuit <- gotQuitEvent
-      unless shouldQuit action
-    gotQuitEvent = do
-      ev <- pollEventWithImGui
-      case ev of
+    handleEvents = do
+      ev' <- pollEventWithImGui
+      case ev' of
         Nothing -> return False
-        Just event -> (isQuit event ||) <$> gotQuitEvent
-    isQuit event = eventPayload event == QuitEvent
+        Just ev
+          | eventPayload ev == QuitEvent -> return True
+          | otherwise -> onEvent (eventPayload ev) >> return False
+    onEvent (WindowResizedEvent r) =
+      let V2 w h = windowResizedEventSize r
+       in glViewport 0 0 w h
+    onEvent _ = pure ()
 
 mainWidget :: IO ()
 mainWidget = do
