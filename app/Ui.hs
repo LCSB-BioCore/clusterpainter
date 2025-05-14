c2124cc2050cf59eab7e371372f175e741e7361c
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2025-05-14 12:05:31 +0200

    actually draw stuff

--- app/Ui.hs
+++ app/Ui.hs
@@ -6,12 +6,26 @@ import Lensref
 import St
 
 import Control.Monad (when)
+import qualified Data.Vector as V
 import DearImGui
 import Graphics.Gloss.Data.Color
+import Graphics.Gloss.Data.Picture hiding (text)
 import Graphics.Gloss.Rendering
 import SDL
 
-renderApp xs ys appst = pure $ (greyN 0.2, Circle 30)
+renderApp xs ys appst = withVal appst $ \st -> pure (greyN 0.2, pic st)
+  where
+    pic st =
+      Scale 30 30
+        $ Pictures
+        $ map
+            (\c ->
+               uncurry translate (position c)
+                 $ color (greyN $ features c V.! 0)
+                 $ circleSolid
+                 $ weight c / 2)
+        $ V.toList
+        $ clusters st
 
 onEvent _ _ = pure ()
 
